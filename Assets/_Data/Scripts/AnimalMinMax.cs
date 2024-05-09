5d83bef93777d8c4a3cdf8e6d6b13b4bcf1ef777
Sai <tranminhduc18116@gmail.com>
2024-05-09 12:06:51 +0700

    e29

--- Assets/_Data/Scripts/AnimalMinMax.cs
+++ Assets/_Data/Scripts/AnimalMinMax.cs
@@ -4,13 +4,18 @@ using UnityEngine;
 
 public class AnimalMinMax : MonoBehaviour
 {
+    [SerializeField] protected AnimalManager animalManager;
     [SerializeField] protected AnimalSorting animalSorting;
+    [SerializeField] protected Animal animalMaxSorting;
+    [SerializeField] protected Animal animalMinSorting;
+
     [SerializeField] protected Animal animalMax;
     [SerializeField] protected Animal animalMin;
 
     private void Start()
     {
-        this.GetAnimals();
+        this.GetMinMaxFromManager();
+        this.GetMinMaxFromSorting();
     }
 
     private void Reset()
@@ -21,34 +26,89 @@ public class AnimalMinMax : MonoBehaviour
     protected void LoadComponents()
     {
         this.LoadAnimalManager();
+        this.LoadAnimalSorting();
     }
 
-    protected void LoadAnimalManager()
+    protected void LoadAnimalSorting()
     {
         this.animalSorting = GetComponent<AnimalSorting>();
     }
 
-    protected void GetAnimals()
+    protected void LoadAnimalManager()
+    {
+        this.animalManager = GetComponent<AnimalManager>();
+    }
+
+    protected void GetMinMaxFromSorting()
     {
         if (this.animalSorting.SortByWeight.Count == 0)
         {
-            Debug.Log("Recursive: GetAnimals");
-            Invoke(nameof(this.GetAnimals), 1);
+            Debug.Log("Recursive: GetMinMaxFromSorting");
+            Invoke(nameof(this.GetMinMaxFromSorting), 1);
             return;
         }
 
-        this.GetAnimalMax();
-        this.GetAnimalMin();
+        this.GetAnimalMaxFromSorting();
+        this.GetAnimalMinFromSorting();
     }
 
-    protected void GetAnimalMax()
+    protected void GetAnimalMaxFromSorting()
     {
         int animalCount = this.animalSorting.SortByWeight.Count;
-        this.animalMax = this.animalSorting.SortByWeight[animalCount - 1];
+        this.animalMaxSorting = this.animalSorting.SortByWeight[animalCount - 1];
     }
 
-    protected void GetAnimalMin()
+    protected void GetAnimalMinFromSorting()
     {
-        this.animalMin = this.animalSorting.SortByWeight[0];
+        this.animalMinSorting = this.animalSorting.SortByWeight[0];
+    }
+
+    protected void GetMinMaxFromManager()
+    {
+        long startTime = UnixTime.GetUnixTimeMicro();
+
+        if (this.animalManager.Animals.Count == 0)
+        {
+            Debug.Log("Recursive: GetMinMaxFromManager");
+            Invoke(nameof(this.GetMinMaxFromManager), 1);
+            return;
+        }
+
+        foreach (Animal animal in this.animalManager.Animals)
+        {
+            this.CheckMaxAnimal(animal);
+            this.CheckMinAnimal(animal);
+        }
+
+        float timeDiff = UnixTime.GetTimeDiffToNow(startTime);
+        Debug.Log("== GetMinMaxFromManager timeDiff: " + timeDiff);
+    }
+
+    protected void CheckMaxAnimal(Animal animal)
+    {
+        if (this.animalMax == null)
+        {
+            this.animalMax = animal;
+            return;
+        }
+
+        if (this.animalMax.GetWeight() < animal.GetWeight())
+        {
+            this.animalMax = animal;
+        }
+    }
+
+    protected void CheckMinAnimal(Animal animal)
+    {
+        if (this.animalMin == null)
+        {
+            this.animalMin = animal;
+            return;
+        }
+
+        if (this.animalMin.GetWeight() > animal.GetWeight())
+        {
+            this.animalMin = animal;
+        }
     }
 }
